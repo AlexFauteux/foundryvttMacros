f31f9218bfec0124acd2017bef293611cedd6262
Alexandre Fauteux <2643722@gmail.com>
2025-03-07 14:55:57 -0500

    Finished CR 5 to 10

--- DND5E/CR_5_10/TreasureHoard_CR_5_10_Implements.js
+++ DND5E/CR_5_10/TreasureHoard_CR_5_10_Implements.js
@@ -2,23 +2,26 @@
 async function rarityRoll(){ 
     let rarity = new Roll("1d100");
     await rarity.evaluate();
-    // Common =1, Uncommon = 2 and Rare = 3
-    if (rarity.total < 54) {
-     return 1;
+    // Common =1, Uncommon = 2, Rare = 3 and Very Rare = 4
+    if (rarity.total > 1 && rarity.total <= 30) {
+        return 1;
     }
-    else if (rarity.total > 54 && rarity.total < 92){
-     return 2;
+    else if (rarity.total >= 31 && rarity.total <= 81){
+        return 2;
     }
-    else {
-     return 3;
+    else if (rarity.total >= 82 && rarity.total <= 98){
+        return 3;
     } 
+    else {
+        return 4;
+    }
     // this is impossible.
     return 0;
 }
 
 // set scoped variables
-let coins = new Roll("2d4 * 100");
-let numMaItems = new Roll("1d4-1");
+let coins = new Roll("8d10 * 100");
+let numMaItems = new Roll("1d3");
 let itemRarities = [];
 let items = [];
    
@@ -35,19 +38,24 @@ if (numMaItems.total > 0) {
     for (let itemRarity of itemRarities) {
         switch (itemRarity) {
             case 1:
-                let table1 = await fromUuid("Compendium.dnd-dungeon-masters-guide.tables.RollTable.dmgRelicsCommon0");
+                let table1 = await fromUuid("Compendium.dnd-dungeon-masters-guide.tables.RollTable.dmgImplementsCom");
                 let result1 = await table1.roll();
                 items.push(result1.results[0].text);
                 break;
             case 2:
-                let table2 = await fromUuid("Compendium.dnd-dungeon-masters-guide.tables.RollTable.dmgRelicsUncommo");
+                let table2 = await fromUuid("Compendium.dnd-dungeon-masters-guide.tables.RollTable.dmgImplementsUnc");
                 let result2 = await table2.roll();
                 items.push(result2.results[0].text);
                 break;
             case 3:
-                let table3 = await fromUuid("Compendium.dnd-dungeon-masters-guide.tables.RollTable.dmgRelicsRare000");
+                let table3 = await fromUuid("Compendium.dnd-dungeon-masters-guide.tables.RollTable.dmgImplementsRar");
                 let result3 = await table3.roll();
                 items.push(result3.results[0].text);
+                break;
+            case 4:
+                let table4 = await fromUuid("Compendium.dnd-dungeon-masters-guide.tables.RollTable.dmgImplementsVer");
+                let result4 = await table4.roll();
+                items.push(result4.results[0].text);
                 break;
             default:
                 break;
